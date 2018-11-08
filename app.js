77fd86fc21b93d8d982cd432910226a6155af616
Emily Doan <emilydoan52@gmail.com>
2018-11-07 18:18:37 -0800

    click function now works to change animate and still

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var topics = ["mario", "donkey kong", "link", "samus", "yoshi", "kirby", "fox", "pikachu", "luigi"];
+
 $(document).ready(function () {
 
-    //m5yQIDCRuAIrUCrGRJcCYGp2STyLBjPq
+    var topics = ["mario", "donkey kong", "link", "samus", "yoshi", "kirby", "fox", "pikachu", "luigi"];
+
     function makebutton() {
         for (i = 0; i < topics.length; i++) {
             var characters = $("<button>");
@@ -28,30 +29,107 @@ $(document).ready(function () {
                 console.log(response);
                 // Saving the image_original_url property
                 for (i = 0; i < response.data.length; i++) {
-                    var imageUrl = response.data[i].images.original.url;
+                    var imageUrl = response.data[i].images.fixed_height_still.url;
+                    var imageGif = response.data[i].images.original.url;
                     var rating = response.data[i].rating;
 
 
                     // Creating and storing an image tag
                     var characterImage = $("<img>");
                     var ratingContainer = $("<p>");
-                    var characterBox = $("<div>");
+
 
 
                     // Setting the catImage src attribute to imageUrl
                     characterImage.attr("src", imageUrl);
-                    characterImage.attr("alt", "character image");
+                    characterImage.attr("data-still", imageUrl);
+                    characterImage.attr("data-animate", imageGif);
+                    characterImage.attr("data-state", "still")
+                    // characterImage.attr("alt", "character image");
+                    characterImage.addClass("gif");
                     ratingContainer.text("Rating: " + rating);
 
                     // Prepending the catImage to the images div
-                    characterBox.append(characterImage);
-                    characterBox.append(ratingContainer);
+                    $("#images").prepend(characterImage);
+                    $("#images").prepend(ratingContainer);
 
-                    $("#images").prepend(characterBox);
+                    // $("#images").prepend(characterImage);
                     // });
 
                 };
 
             });
     });
+    $("#submit").on("click", function () {
+        var input = $("#inputBox").val().trim();
+        topics.push(input);
+        console.log(input);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=m5yQIDCRuAIrUCrGRJcCYGp2STyLBjPq&q=" + input + "&limit=10&offset=0rating=G &lang=en";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(queryURL);
+                console.log(response);
+
+
+                var characters = $("<button>");
+                characters.text(input);
+                $("#wordbuttons").append(characters);
+
+                // Saving the image_original_url property
+                for (i = 0; i < response.data.length; i++) {
+                    var imageUrl = response.data[i].images.fixed_height_still.url;
+                    var imageGif = response.data[i].images.original.url;
+                    var rating = response.data[i].rating;
+
+
+                    // Creating and storing an image tag
+                    var characterImage = $("<img>");
+                    var ratingContainer = $("<p>");
+
+
+
+                    // Setting the catImage src attribute to imageUrl
+                    characterImage.attr("src", imageUrl);
+                    characterImage.attr("data-still", imageUrl);
+                    characterImage.attr("data-animate", imageGif);
+                    characterImage.attr("data-state", "still")
+                    // characterImage.attr("alt", "character image");
+                    characterImage.addClass("gif");
+                    ratingContainer.text("Rating: " + rating);
+
+                    // Prepending the catImage to the images div
+                    $("#images").prepend(characterImage);
+                    $("#images").prepend(ratingContainer);
+
+                    // $("#images").prepend(characterBox);
+                };
+
+
+            })
+
+
+
+
+
+    });
+    $(document).on("click", ".gif", function () {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        console.log("hello world");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+
 });
